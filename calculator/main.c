fc5d10ad48c0674a0ad08cbc932c3ed260b0d2e2
DeniseLuiz <denisesantos.luiz@usp.br>
2021-03-31 21:10:01 -0300

    do while

--- calculator/main.c
+++ calculator/main.c
@@ -3,6 +3,40 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
+	// Definindo variáveis:
+	int num1;
+	int num2;
+	char operacao;
+	int cont;
+	
+do{
+	
+	// Solicitando ao usuário 
+	printf("Calculadora\n");
+	
+	printf("Entre com o primeiro valor inteiro");
+	scanf("%i\n",&num1);
+
+	printf("Entre com o segundo valor inteiro");
+	scanf("%i\n",&num2);
+	
+	fflush(stdin);
+	printf("Entre com o sinal de operacao +, -, * ou /:");
+	scanf("%c",&operacao);
+	
+	//Aplicando switch
+	
+}
+ while (cont ==1);{
+	printf("Deseja continuar? 1 - sim ou 2 - nao\n");
+	scanf("%i\n", cont);
+}
+				//{
+			//	printf("Recomecando");
+
+	
+	
 	return 0;
 }
