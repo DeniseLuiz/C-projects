331ea3205b458b1bf40756717ad99235651b394c
DeniseLuiz <denisesantos.luiz@usp.br>
2021-04-01 20:42:01 -0300

    positives or not

---
+++ positives or not/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int vetor [10];
+	int i; 
+	int pos=0;
+	int neg=0; 
+	int nul=0;
+	
+	
+	for (i=0; i<10; i++){
+		
+		printf ("Entre com o valor %i\n", i+1);
+		scanf ("%i", &vetor[i]);
+
+	}
+	
+	
+
+
