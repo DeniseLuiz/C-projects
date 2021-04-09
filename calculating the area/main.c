94b0efc075fe4545d74145b58a5fe5e4a02fee79
DeniseLuiz <denisesantos.luiz@usp.br>
2021-04-09 17:13:59 -0300

    calculating the area

---
+++ calculating the area/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])
+ {
+ 	// Definindo variáveis:
+ 	int base;
+ 	int altura;
+ 	int area;
+ 	
+ 	// Definindo os valores pelo usuários:
+ 	printf("Digite o valor da base");
+ 	scanf("%i", &base);
+ 	
+ 	printf("Digite o valor da altura");
+ 	scanf ("%i", &altura);
+ 
+}
